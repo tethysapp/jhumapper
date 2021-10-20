c799dd36d868ca0435a0b2cd5c4d9f15f770d333
Riley Hales <rileyhales1@gmail.com>
2021-10-20 15:12:23 -0600

    increment version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces'
 
 setup(
     name=release_package,
-    version='0.1.0',
+    version='0.2.0',
     description='',
     long_description='',
     keywords='',
